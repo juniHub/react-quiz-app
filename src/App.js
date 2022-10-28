fb098966462ad86986cd4bd5c861edba24840b9e
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2022-10-28 06:07:34 -0700

    better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import React, { useState } from "react";
 import { data } from "./data";
+import Footer from "./Components/Footer";
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -60,33 +61,13 @@ export default function App() {
           </button>
 
         </div>
-        <footer className="m-4 text-center">
-            Made with ReactJS and TailwindCSS by{" "}
-            <a
-              className="text-pink-400"
-              href="https://www.juninguyen.tech/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Juni Nguyen
-            </a>{" "}
-            <br/>
-            Question and Answer Database from{" "}
-            <a
-              className="text-pink-400"
-              href="https://github.com/jmatzen/quizsail"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Here
-            </a>{" "}
-          </footer>
+        <Footer/>
       </div>
     );
   } else {
     return (
       <div className="flex flex-col min-h-screen container m-4 p-4 mx-auto items-center">
-        <div className="wrapper flex-grow">
+        <div className="wrapper flex-grow w-full">
           <h2 className="mb-6 p-4 h-30 text-center text-2xl font-bold">
             {data[currentQuestion].q}
           </h2>
@@ -99,7 +80,7 @@ export default function App() {
           </span>
           </div>
           {data[currentQuestion].c.map((variant) => (
-            <div className="m-2 h-14 border-2 border-black mx-auto text-center">
+            <div className="m-2 h-14 border-2 border-black text-center">
               <p
                 key={variant.id}
                                 
@@ -117,7 +98,10 @@ export default function App() {
             </div>
           ))}
 
-          {clickAnswer && myAnswer !== data[currentQuestion].a && (
+          
+        </div>
+
+        {clickAnswer && myAnswer !== data[currentQuestion].a && (
             <button
               className="w-full h-14 mt-2 px-2 rounded-lg bg-gray-200 text-blue-400 font-bold hover:bg-gray-400 hover:text-blue-600"
               onClick={() => showAnswer()}
@@ -130,6 +114,8 @@ export default function App() {
               Correct Answer: {data[currentQuestion].a}
             </p>
           )}
+
+       
 
           {currentQuestion < data.length - 1 && (
             <button
@@ -157,9 +143,7 @@ export default function App() {
             </button>
           )}
 
-
-
-
+  
           {currentQuestion === data.length - 1 && (
             <button
               className="w-full h-14 mt-2 px-2 rounded-lg bg-gray-600 text-pink-400 font-bold hover:bg-gray-800 hover:text-pink-600"
@@ -169,29 +153,10 @@ export default function App() {
             </button>
           )}
 
-          
-        </div>
-        <footer className="m-4 text-center">
-            Made with ReactJS and TailwindCSS by{" "}
-            <a
-              className="text-pink-400"
-              href="https://www.juninguyen.tech/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Juni Nguyen
-            </a>{" "}
-            <br/>
-            Question and Answer Database from{" "}
-            <a
-              className="text-pink-400"
-              href="https://github.com/jmatzen/quizsail"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Here
-            </a>{" "}
-          </footer>
+
+         <Footer/>
+
+       
       </div>
     );
   }
---
+++ src/Components/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Footer() {
+    
+      return (
+       
+          
+          <footer className="m-4 text-center">
+              Made with ReactJS and TailwindCSS by{" "}
+              <a
+                className="text-pink-400"
+                href="https://www.juninguyen.tech/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Juni Nguyen
+              </a>{" "}
+              <br/>
+              Question and Answer Database from{" "}
+              <a
+                className="text-pink-400"
+                href="https://github.com/jmatzen/quizsail"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Here
+              </a>{" "}
+            </footer>
+      
+      )
+}
